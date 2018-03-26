298e8dc5bbd555c7df4bd3a024d4bc61cf261094
leftthomas <leftthomas@qq.com>
2018-03-27 04:25:28 +0800

    finish svhn

--- datasets/svhn.py
+++ datasets/svhn.py
@@ -2,6 +2,7 @@ import os
 import os.path
 
 import numpy as np
+import torch
 import torch.utils.data as data
 from PIL import Image
 
@@ -48,6 +49,21 @@ class SVHN(data.Dataset):
         self.labels = loaded_mat['y'].astype(np.int64).squeeze()
         np.place(self.labels, self.labels == 10, 0)
         self.data = np.transpose(self.data, (3, 2, 0, 1))
+
+        if mode == 'test_multi':
+            self.data = torch.from_numpy(self.data)
+            self.labels = torch.from_numpy(self.labels)
+            idx = torch.randperm(len(self.data))
+            self.data = torch.cat([self.data, torch.index_select(self.data, dim=0, index=idx)], dim=-1)
+            self.labels = torch.stack([self.labels, torch.index_select(self.labels, dim=0, index=idx)]).t()
+            # make sure the two number is different
+            mask = torch.ne(self.labels[:, 0], self.labels[:, 1])
+            self.data = self.data.masked_select(
+                mask.unsqueeze(dim=-1).unsqueeze(dim=-1).unsqueeze(dim=-1)).view(-1, 3, 32, 64)
+            self.labels = self.labels.masked_select(mask.unsqueeze(dim=-1)).view(-1, 2)
+            # just compare the labels, don't compare the order
+            self.data, _ = self.labels.sort(dim=-1)
+            self.data = self.data.numpy()
 
     def __getitem__(self, index):
         img, target = self.data[index], self.labels[index]
