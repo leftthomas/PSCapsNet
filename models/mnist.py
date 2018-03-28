83d1ded9f3d47e7c517348c3b63e155dea211c71
leftthomas <leftthomas@qq.com>
2018-03-28 15:57:13 +0800

    use in_length = 32

--- models/mnist.py
+++ models/mnist.py
@@ -2,27 +2,23 @@ import torch.nn.functional as F
 from capsule_layer import CapsuleLinear
 from torch import nn
 
-from resnet import resnet20
-
 
 class MNISTNet(nn.Module):
     def __init__(self, num_iterations=3, net_mode='Capsule'):
         super(MNISTNet, self).__init__()
 
         self.net_mode = net_mode
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        layers = []
-        for name, module in resnet20().named_children():
-            if name == 'conv1' or isinstance(module, nn.AvgPool2d) or isinstance(module, nn.Linear):
-                continue
-            layers.append(module)
-        self.features = nn.Sequential(*layers)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.features = nn.Sequential(nn.ReLU(), nn.Conv2d(16, 16, kernel_size=3, padding=1),
+                                      nn.ReLU(), nn.AvgPool2d(kernel_size=2),
+                                      nn.Conv2d(16, 32, kernel_size=3, padding=1),
+                                      nn.ReLU(), nn.Conv2d(32, 32, kernel_size=3, padding=1), nn.ReLU())
         if self.net_mode == 'Capsule':
-            self.classifier = CapsuleLinear(out_capsules=10, in_length=2, out_length=4, in_capsules=None,
+            self.classifier = CapsuleLinear(out_capsules=10, in_length=32, out_length=4, in_capsules=None,
                                             routing_type='contract', share_weight=True, num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
-            self.classifier = nn.Linear(in_features=64, out_features=10)
+            self.classifier = nn.Linear(in_features=32, out_features=10)
 
     def forward(self, x):
         out = self.conv1(x)
@@ -30,7 +26,7 @@ class MNISTNet(nn.Module):
 
         if self.net_mode == 'Capsule':
             out = out.permute(0, 2, 3, 1)
-            out = out.contiguous().view(out.size(0), -1, 2)
+            out = out.contiguous().view(out.size(0), -1, 32)
             out = self.classifier(out)
             classes = out.norm(dim=-1)
         else:
