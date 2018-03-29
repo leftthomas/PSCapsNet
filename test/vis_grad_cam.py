fc9ebdbe66e5f51f17bf8491a7351eac03f457e9
leftthomas <leftthomas@qq.com>
2018-03-29 16:30:38 +0800

    add test vis grad_cam

--- test/vis_grad_cam.py
+++ test/vis_grad_cam.py
@@ -6,7 +6,7 @@ from torchvision.models import vgg19
 from utils import GradCam
 
 if __name__ == '__main__':
-    test_image = (transforms.ToTensor()(Image.open('both.png'))).unsqueeze(dim=0)
+    test_image = (transforms.ToTensor()(Image.open('test/both.png'))).unsqueeze(dim=0)
     model = vgg19(pretrained=True)
     model.__setattr__(name='net_mode', value='CNN')
     if torch.cuda.is_available():
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     grad_cam = GradCam(model)
     feature_image = grad_cam(test_image).squeeze(dim=0)
     feature_image = transforms.ToPILImage()(feature_image)
-    feature_image.save('grad_cam.png')
+    feature_image.save('test/grad_cam.png')
