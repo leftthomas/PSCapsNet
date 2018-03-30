aaaa045f4dc7755debdf8d3f69bf06232b359d4b
leftthomas <leftthomas@qq.com>
2018-03-30 23:01:08 +0800

    scale model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -8,18 +8,18 @@ class FashionMNISTNet(nn.Module):
         super(FashionMNISTNet, self).__init__()
 
         self.net_mode = net_mode
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, padding=1), nn.ReLU())
-        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(),
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 32, kernel_size=3, padding=1), nn.ReLU())
+        self.features = nn.Sequential(nn.Conv2d(32, 32, kernel_size=3, padding=1), nn.ReLU(),
                                       nn.AvgPool2d(kernel_size=2),
-                                      nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU())
+                                      nn.Conv2d(32, 64, kernel_size=3, padding=1), nn.ReLU(),
+                                      nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU())
         if self.net_mode == 'Capsule':
-            self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type='dynamic',
+            self.classifier = CapsuleLinear(out_capsules=10, in_length=64, out_length=16, routing_type='dynamic',
                                             num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
-            self.classifier = nn.Sequential(nn.Linear(in_features=128, out_features=128), nn.ReLU(),
-                                            nn.Linear(in_features=128, out_features=10))
+            self.classifier = nn.Sequential(nn.Linear(in_features=64, out_features=64), nn.ReLU(),
+                                            nn.Linear(in_features=64, out_features=10))
 
     def forward(self, x):
         out = self.conv1(x)
@@ -27,7 +27,7 @@ class FashionMNISTNet(nn.Module):
 
         if self.net_mode == 'Capsule':
             out = out.permute(0, 2, 3, 1)
-            out = out.contiguous().view(out.size(0), -1, 128)
+            out = out.contiguous().view(out.size(0), -1, 64)
             out = self.classifier(out)
             classes = out.norm(dim=-1)
         else:
