6bcba845de01eccd83541efb79c58e016e36cda4
leftthomas <leftthomas@qq.com>
2018-04-03 01:24:07 +0800

    change cnn model

--- models/mnist.py
+++ models/mnist.py
@@ -17,8 +17,8 @@ class MNISTNet(nn.Module):
                                             num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
-            self.classifier = nn.Sequential(nn.Linear(in_features=64, out_features=32), nn.ReLU(),
-                                            nn.Linear(in_features=32, out_features=10))
+            self.classifier = nn.Sequential(nn.Linear(in_features=64, out_features=128), nn.ReLU(),
+                                            nn.Linear(in_features=128, out_features=10))
 
     def forward(self, x):
         out = self.conv1(x)
