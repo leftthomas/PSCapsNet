ceb3690845de32eaf8d9ced9272c994bbc3ef0fc
leftthomas <leftthomas@qq.com>
2018-04-02 21:15:15 +0800

    change in length

--- models/mnist.py
+++ models/mnist.py
@@ -13,7 +13,7 @@ class MNISTNet(nn.Module):
                                       nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
                                       nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1), nn.ReLU())
         if self.net_mode == 'Capsule':
-            self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type=routing_type,
+            self.classifier = CapsuleLinear(out_capsules=10, in_length=64, out_length=16, routing_type=routing_type,
                                             num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
@@ -26,7 +26,7 @@ class MNISTNet(nn.Module):
 
         if self.net_mode == 'Capsule':
             out = out.permute(0, 2, 3, 1)
-            out = out.contiguous().view(out.size(0), -1, 128)
+            out = out.contiguous().view(out.size(0), -1, 64)
             out = self.classifier(out)
             classes = out.norm(dim=-1)
         else:
