43d599d42b74699ccb3082bf584016323b2a308e
leftthomas <leftthomas@qq.com>
2018-04-02 15:46:24 +0800

    change margin loss

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ class MarginLoss(nn.Module):
         super(MarginLoss, self).__init__()
 
     def forward(self, classes, labels):
-        left = F.relu(0.95 - classes, inplace=True) ** 2
+        left = F.relu(0.9 - classes, inplace=True) ** 2
         right = F.relu(classes - 0.1, inplace=True) ** 2
         loss = labels * left + 0.25 * (1 - labels) * right
         return loss.mean()
