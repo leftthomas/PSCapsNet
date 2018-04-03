858292c302e6900d802e0c1ab060a017caf2c870
leftthomas <leftthomas@qq.com>
2018-04-04 00:57:58 +0800

    add stride

--- models/svhn.py
+++ models/svhn.py
@@ -9,11 +9,9 @@ class SVHNNet(nn.Module):
 
         self.net_mode = net_mode
         self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, padding=1), nn.ReLU())
-        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.AvgPool2d(kernel_size=2),
+        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1), nn.ReLU(),
                                       nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.AvgPool2d(kernel_size=2))
+                                      nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1), nn.ReLU())
         if self.net_mode == 'Capsule':
             self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type=routing_type,
                                             num_iterations=num_iterations)
