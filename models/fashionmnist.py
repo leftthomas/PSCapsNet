0900647e9c026992308a390f95aac6839f717451
leftthomas <leftthomas@qq.com>
2018-04-03 17:32:44 +0800

    change model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -8,10 +8,11 @@ class FashionMNISTNet(nn.Module):
         super(FashionMNISTNet, self).__init__()
 
         self.net_mode = net_mode
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, padding=1), nn.ReLU())
-        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1), nn.ReLU(),
-                                      nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1), nn.ReLU())
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3), nn.ReLU())
+        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3), nn.ReLU(),
+                                      nn.AvgPool2d(kernel_size=2),
+                                      nn.Conv2d(64, 128, kernel_size=3), nn.ReLU(),
+                                      nn.Conv2d(128, 128, kernel_size=3), nn.ReLU())
         if self.net_mode == 'Capsule':
             self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type=routing_type,
                                             num_iterations=num_iterations)
