61bec98c613ef89bcee0844a9f5592e24da1bb0f
leftthomas <leftthomas@qq.com>
2018-04-04 00:58:53 +0800

    use AvgPool2d

--- models/cifar10.py
+++ models/cifar10.py
@@ -12,7 +12,8 @@ class CIFAR10Net(nn.Module):
         self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(),
                                       nn.AvgPool2d(kernel_size=2),
                                       nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU())
+                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU(),
+                                      nn.AvgPool2d(kernel_size=2))
         if self.net_mode == 'Capsule':
             self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type=routing_type,
                                             num_iterations=num_iterations)
