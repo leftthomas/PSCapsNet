b46d6e6a06ae8f6067656ec30087b057be4f00f5
leftthomas <leftthomas@qq.com>
2018-04-05 14:42:08 +0800

    change model

--- models/cifar100.py
+++ models/cifar100.py
@@ -14,9 +14,9 @@ class CIFAR100Net(nn.Module):
                                       nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1), nn.ReLU())
         if self.net_mode == 'Capsule':
             self.classifier = nn.Sequential(
-                CapsuleLinear(out_capsules=128, in_length=128, out_length=16, routing_type=routing_type,
+                CapsuleLinear(out_capsules=32, in_length=128, out_length=16, routing_type=routing_type,
                               num_iterations=num_iterations, squash=False),
-                CapsuleLinear(out_capsules=100, in_length=16, out_length=32, in_capsules=128, share_weight=False,
+                CapsuleLinear(out_capsules=100, in_length=16, out_length=32, in_capsules=32, share_weight=False,
                               routing_type=routing_type, num_iterations=num_iterations))
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
