7b38f6f34b3b6ec3f48245d489e65ee04ce598a3
leftthomas <leftthomas@qq.com>
2018-04-08 21:56:48 +0800

    change length and add BN

--- models/mnist.py
+++ models/mnist.py
@@ -8,12 +8,12 @@ class MNISTNet(nn.Module):
         super(MNISTNet, self).__init__()
 
         self.net_mode = net_mode
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3), nn.ReLU())
-        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2), nn.ReLU(),
-                                      nn.Conv2d(64, 128, kernel_size=3), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3), nn.ReLU())
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3), nn.BatchNorm2d(64), nn.ReLU())
+        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2), nn.BatchNorm2d(64), nn.ReLU(),
+                                      nn.Conv2d(64, 128, kernel_size=3), nn.BatchNorm2d(128), nn.ReLU(),
+                                      nn.Conv2d(128, 128, kernel_size=3), nn.BatchNorm2d(128), nn.ReLU())
         if self.net_mode == 'Capsule':
-            self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type=routing_type,
+            self.classifier = CapsuleLinear(out_capsules=10, in_length=64, out_length=16, routing_type=routing_type,
                                             num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
@@ -26,7 +26,7 @@ class MNISTNet(nn.Module):
 
         if self.net_mode == 'Capsule':
             out = out.permute(0, 2, 3, 1)
-            out = out.contiguous().view(out.size(0), -1, 128)
+            out = out.contiguous().view(out.size(0), -1, 64)
             out = self.classifier(out)
             classes = out.norm(dim=-1)
         else:
