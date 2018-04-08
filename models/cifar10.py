53a28a13021e5b177170245b7f9bfc0ba7fdc74e
leftthomas <leftthomas@qq.com>
2018-04-08 12:34:39 +0800

    add BN, resnet

--- models/cifar10.py
+++ models/cifar10.py
@@ -8,11 +8,11 @@ class CIFAR10Net(nn.Module):
         super(CIFAR10Net, self).__init__()
 
         self.net_mode = net_mode
-        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, padding=1), nn.ReLU())
-        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(),
+        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, padding=1), nn.BatchNorm2d(64), nn.ReLU())
+        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),
                                       nn.AvgPool2d(kernel_size=2),
-                                      nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU(),
+                                      nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
+                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
                                       nn.AvgPool2d(kernel_size=2))
         if self.net_mode == 'Capsule':
             self.classifier = CapsuleLinear(out_capsules=10, in_length=64, out_length=16, routing_type=routing_type,
