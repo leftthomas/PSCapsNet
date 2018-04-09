5643c0391e98306ededc80b5c54560358447a4d6
leftthomas <leftthomas@qq.com>
2018-04-09 16:22:39 +0800

    fix bug

--- models/stl10.py
+++ models/stl10.py
@@ -11,17 +11,16 @@ class STL10Net(nn.Module):
         self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3), nn.BatchNorm2d(64), nn.ReLU())
         self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2), nn.BatchNorm2d(64), nn.ReLU(),
                                       nn.Conv2d(64, 128, kernel_size=3), nn.BatchNorm2d(128), nn.ReLU(),
+                                      nn.Conv2d(128, 128, kernel_size=3, stride=2), nn.BatchNorm2d(128), nn.ReLU(),
+                                      nn.Conv2d(128, 128, kernel_size=3), nn.BatchNorm2d(128), nn.ReLU(),
                                       nn.Conv2d(128, 128, kernel_size=3, stride=2), nn.BatchNorm2d(128), nn.ReLU())
         if self.net_mode == 'Capsule':
-            self.classifier = nn.Sequential(
-                CapsuleLinear(out_capsules=128, in_length=64, out_length=16, routing_type=routing_type,
-                              num_iterations=num_iterations, squash=False),
-                CapsuleLinear(out_capsules=10, in_length=16, out_length=8, routing_type=routing_type,
-                              num_iterations=num_iterations))
+            self.classifier = CapsuleLinear(out_capsules=10, in_length=64, out_length=16, routing_type=routing_type,
+                                            num_iterations=num_iterations)
         else:
-            self.pool = nn.AdaptiveAvgPool2d(output_size=4)
-            self.classifier = nn.Sequential(nn.Linear(in_features=2048, out_features=512), nn.ReLU(),
-                                            nn.Linear(in_features=512, out_features=10))
+            self.pool = nn.AdaptiveAvgPool2d(output_size=1)
+            self.classifier = nn.Sequential(nn.Linear(in_features=128, out_features=128), nn.ReLU(),
+                                            nn.Linear(in_features=128, out_features=10))
 
     def forward(self, x):
         out = self.conv1(x)
