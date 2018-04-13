1efd745485cd6ad3bab107bcb24147b5c89746ef
leftthomas <leftthomas@qq.com>
2018-04-13 21:54:03 +0800

    change model

--- models/stl10.py
+++ models/stl10.py
@@ -24,8 +24,8 @@ class STL10Net(nn.Module):
                                             num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
-            self.classifier = nn.Sequential(nn.Linear(in_features=64, out_features=64), nn.ReLU(),
-                                            nn.Linear(in_features=64, out_features=10))
+            self.classifier = nn.Sequential(nn.Linear(in_features=128, out_features=128), nn.ReLU(),
+                                            nn.Linear(in_features=128, out_features=10))
 
     def forward(self, x):
         out = self.conv1(x)
