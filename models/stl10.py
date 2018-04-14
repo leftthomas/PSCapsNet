6b7f780b6500193a557491c488aded4d2f7a5704
leftthomas <leftthomas@qq.com>
2018-04-14 13:35:04 +0800

    revert

--- models/stl10.py
+++ models/stl10.py
@@ -20,12 +20,12 @@ class STL10Net(nn.Module):
         self.features = nn.Sequential(*layers)
 
         if self.net_mode == 'Capsule':
-            self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, routing_type=routing_type,
+            self.classifier = CapsuleLinear(out_capsules=10, in_length=32, out_length=8, routing_type=routing_type,
                                             num_iterations=num_iterations)
         else:
             self.pool = nn.AdaptiveAvgPool2d(output_size=1)
-            self.classifier = nn.Sequential(nn.Linear(in_features=128, out_features=128), nn.ReLU(),
-                                            nn.Linear(in_features=128, out_features=10))
+            self.classifier = nn.Sequential(nn.Linear(in_features=64, out_features=64), nn.ReLU(),
+                                            nn.Linear(in_features=64, out_features=10))
 
     def forward(self, x):
         out = self.conv1(x)
@@ -33,7 +33,7 @@ class STL10Net(nn.Module):
 
         if self.net_mode == 'Capsule':
             out = out.permute(0, 2, 3, 1)
-            out = out.contiguous().view(out.size(0), -1, 128)
+            out = out.contiguous().view(out.size(0), -1, 32)
             out = self.classifier(out)
             classes = out.norm(dim=-1)
         else:
