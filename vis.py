28188c5f65923948a3201d50c39b0ab9f07da497
leftthomas <leftthomas@qq.com>
2018-04-20 22:48:54 +0800

    make code better

--- vis.py
+++ vis.py
@@ -24,12 +24,22 @@ if __name__ == '__main__':
         model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location='cpu'))
 
     images, labels = next(iter(get_iterator(DATA_TYPE, 'test_multi', 16, True)))
-    save_image(images, filename='vis_original.png', nrow=4, normalize=True)
+    save_image(images, filename='vis_%s_original.png' % DATA_TYPE, nrow=4, normalize=True)
     if torch.cuda.is_available():
         images = images.cuda()
     images = Variable(images)
 
     for name, module in model.named_children():
-        if name != 'classifier':
+        if name == 'conv1':
             images = module(images)
-            save_image(images.mean(dim=1, keepdim=True).data, filename='vis_%s.png' % name, nrow=4, normalize=True)
+            save_image(images.mean(dim=1, keepdim=True).data, filename='vis_%s_conv1.png' % DATA_TYPE, nrow=4,
+                       normalize=True)
+        elif name == 'features':
+            images = module(images)
+            save_image(images.mean(dim=1, keepdim=True).data, filename='vis_%s_features.png' % DATA_TYPE, nrow=4,
+                       normalize=True)
+        elif name == 'classifier':
+            images = images.permute(0, 2, 3, 1)
+            images = images.contiguous().view(images.size(0), -1, module.weight.size(-1))
+            images = module(images)
+            classes = images.norm(dim=-1)
