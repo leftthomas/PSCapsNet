1df23876f43702688c9444509741849b04877128
leftthomas <leftthomas@qq.com>
2018-04-21 01:33:09 +0800

    finish first draft sp capsnet vis

--- vis.py
+++ vis.py
@@ -1,6 +1,11 @@
 import argparse
 
+import cv2
+import numpy as np
 import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from capsule_layer.functional import flaser
 from torch.autograd import Variable
 from torchvision.utils import save_image
 
@@ -28,18 +33,48 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         images = images.cuda()
     images = Variable(images)
+    image_size = (images.size(-1), images.size(-2))
 
+    features = None
     for name, module in model.named_children():
         if name == 'conv1':
-            images = module(images)
-            save_image(images.mean(dim=1, keepdim=True).data, filename='vis_%s_conv1.png' % DATA_TYPE, nrow=4,
+            out = module(images)
+            save_image(out.mean(dim=1, keepdim=True).data, filename='vis_%s_conv1.png' % DATA_TYPE, nrow=4,
                        normalize=True)
         elif name == 'features':
-            images = module(images)
-            save_image(images.mean(dim=1, keepdim=True).data, filename='vis_%s_features.png' % DATA_TYPE, nrow=4,
-                       normalize=True)
+            out = module(out)
+            features = out
         elif name == 'classifier':
-            images = images.permute(0, 2, 3, 1)
-            images = images.contiguous().view(images.size(0), -1, module.weight.size(-1))
-            images = module(images)
-            classes = images.norm(dim=-1)
+            out = out.permute(0, 2, 3, 1)
+            out = out.contiguous().view(out.size(0), -1, module.weight.size(-1))
+            priors = (module.weight[None, :, None, :, :] @ out[:, None, :, :, None]).squeeze(dim=-1)
+            output = priors.sum(dim=-2, keepdim=True) / priors.size(1)
+            for r in range(3):
+                logits = (priors * F.normalize(output, p=2, dim=-1)).sum(dim=-1, keepdim=True)
+                probs = F.softmax(logits, dim=1)
+                output = (probs * priors).sum(dim=-2, keepdim=True)
+            classes = flaser(output).squeeze(dim=-2).norm(dim=-1)
+            prob = (probs.expand(*probs.size()[:-1], module.weight.size(-1)) * classes.unsqueeze(dim=-1).unsqueeze(
+                dim=-1)).mean(dim=1)
+            prob = prob.contiguous().view(prob.size(0), *features.size()[-2:], -1)
+            prob = prob.permute(0, 3, 1, 2)
+
+            heat_maps = []
+            for i in range(prob.size(0)):
+                img = images[i].data.cpu().numpy()
+                img = img - np.min(img)
+                if np.max(img) != 0:
+                    img = img / np.max(img)
+                mask = prob[i].mean(dim=0)
+                mask = cv2.resize(mask.data.cpu().numpy(), image_size)
+                mask = mask - np.min(mask)
+                if np.max(mask) != 0:
+                    mask = mask / np.max(mask)
+                heat_map = np.float32(cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET))
+                cam = heat_map + np.float32((np.uint8(img.transpose((1, 2, 0)) * 255)))
+                cam = cam - np.min(cam)
+                if np.max(cam) != 0:
+                    cam = cam / np.max(cam)
+                heat_maps.append(transforms.ToTensor()(cv2.cvtColor(np.uint8(255 * cam), cv2.COLOR_BGR2RGB)))
+            heat_maps = torch.stack(heat_maps)
+            save_image(heat_maps, filename='vis_%s_features.png' % DATA_TYPE, nrow=4, normalize=True)
