615ed807e8380b007afd2ad2a2df8f99d588e5b0
leftthomas <leftthomas@qq.com>
2018-05-07 23:07:55 +0800

    make gen figures more easy

--- gen_figures.py
+++ gen_figures.py
@@ -1,19 +1,14 @@
-import argparse
-
 import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate figures')
-    parser.add_argument('--csv_name', default='experiment_1_iter_3.csv', type=str,
-                        choices=['experiment_1_iter_3.csv', 'experiment_1_iter_10.csv', 'experiment_2_CIFAR10.csv',
-                                 'experiment_2_FashionMNIST.csv', 'experiment_2_MNIST.csv', 'experiment_2_STL10.csv',
-                                 'experiment_2_SVHN.csv', 'experiment_3_CIFAR10.csv', 'experiment_3_FashionMNIST.csv',
-                                 'experiment_3_MNIST.csv', 'experiment_3_STL10.csv', 'experiment_3_SVHN.csv'],
-                        help='csv file name')
-    opt = parser.parse_args()
-    CSV_NAME = opt.csv_name
-    data = pd.read_csv('statistics/' + CSV_NAME)
-    ax = data.plot(x='Epoch')
-    ax.set_ylabel('Accuracy')
-    plt.show()
+    csv_names = ['experiment_1_iter_3.csv', 'experiment_1_iter_10.csv', 'experiment_2_CIFAR10.csv',
+                 'experiment_2_FashionMNIST.csv', 'experiment_2_MNIST.csv', 'experiment_2_STL10.csv',
+                 'experiment_2_SVHN.csv', 'experiment_3_CIFAR10.csv', 'experiment_3_FashionMNIST.csv',
+                 'experiment_3_MNIST.csv', 'experiment_3_STL10.csv', 'experiment_3_SVHN.csv']
+
+    for csv_name in csv_names:
+        data = pd.read_csv('statistics/' + csv_name)
+        ax = data.plot(x='Epoch')
+        ax.set_ylabel('Accuracy')
+        plt.savefig(csv_name.split('.')[0] + '.pdf')
